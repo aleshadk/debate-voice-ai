2a748af5ea757ff86b1acde9cd2142d3f001d36a
aleksey diduh <aleksey.diduh@skyeng.ru>
2025-05-18 23:17:11 -0300

    build: set up backend dist file to deploy

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,21 +1,16 @@
 import "dotenv/config";
 import express from "express";
 import cors from "cors";
-import { AnalyzeDebateAnswerSchema } from './schemas/analyze-debate-answer.schema';
-import { analyzeDebatesViaClaude } from './services/analyze-debates-via-claude';
+import { AnalyzeDebateAnswerSchema } from "./schemas/analyze-debate-answer.schema";
+import { analyzeDebatesViaClaude } from "./services/analyze-debates-via-claude";
 
 const app = express();
 const port = 3000;
 
 const ANTHROPIC_API_KEY = process.env.ANTHROPIC_API_KEY;
 
-if (!ANTHROPIC_API_KEY) {
-  throw new Error("ANTHROPIC_API_KEY is not set");
-}
-
 app.use(cors());
 app.use(express.json());
-
 
 app.post("/api/analyze-answer", async (req, res) => {
   const params = AnalyzeDebateAnswerSchema.safeParse(req.body);
@@ -26,28 +21,24 @@ app.post("/api/analyze-answer", async (req, res) => {
       details: params.error.flatten(),
     });
 
-    return
+    return;
   }
 
   const result = await analyzeDebatesViaClaude({
     language: "ru",
     topic: "Стоит ли ограничивать детей в использовании гаджетов",
-    userAnswer: "Нет, ограничивать детей в использовании гаджетов не нужно. Современные дети буквально рождаются с планшетом в руках — зачем мешать естественному отбору? Даже младенцы интуитивно свайпают экраны, а школьники могут обойти родительский контроль быстрее, чем ты найдёшь кнопку включения. Гаджеты — это их язык, их среда, их сила. Вместо того чтобы забирать устройства, лучше смириться: они всё равно научатся, просто без тебя."
-  })
+    userAnswer:
+      "Нет, ограничивать детей в использовании гаджетов не нужно. Современные дети буквально рождаются с планшетом в руках — зачем мешать естественному отбору? Даже младенцы интуитивно свайпают экраны, а школьники могут обойти родительский контроль быстрее, чем ты найдёшь кнопку включения. Гаджеты — это их язык, их среда, их сила. Вместо того чтобы забирать устройства, лучше смириться: они всё равно научатся, просто без тебя.",
+  });
 
   res.send(result);
 });
 
-
 app.get("/", async (req, res) => {
-  const result = await analyzeDebatesViaClaude({
-    language: "ru",
-    topic: "Стоит ли ограничивать детей в использовании гаджетов",
-    userAnswer: "Нет, ограничивать детей в использовании гаджетов не нужно. Современные дети буквально рождаются с планшетом в руках — зачем мешать естественному отбору? Даже младенцы интуитивно свайпают экраны, а школьники могут обойти родительский контроль быстрее, чем ты найдёшь кнопку включения. Гаджеты — это их язык, их среда, их сила. Вместо того чтобы забирать устройства, лучше смириться: они всё равно научатся, просто без тебя."
-  })
-
-  res.send(result);
-})
+  res.send(
+    `I'm ok. Anthropic API_KEY exists: ${ANTHROPIC_API_KEY !== undefined}`
+  );
+});
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
